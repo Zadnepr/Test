652e19b3a5c2ad95853f15010d89a2a434f149a5
Zadnepr <zadnepr@gmail.com>
2021-05-22 18:13:27 +0300

    Singleton pattern

--- controllers/api/v1/methodRates.php
+++ controllers/api/v1/methodRates.php
@@ -8,17 +8,9 @@ class methodRates extends \app\controllers\method
 {
     public static function doMethod($request=null){
         $get = $request->get();
-        $data = Btc::getData();
+        $currency = $get['currency'] ? explode(',',$get['currency']) : null;
+        $data = Btc::getRates($currency);
         if(!$data) return self::returnError('Data source is missing', 400);
-        array_walk($data, function(&$a, $b) { $a = $a['sell'] * 1.02; });
-        asort ( $data );
-        if($get['currency']){
-            $cur = explode(',',$get['currency']);
-            $data = array_filter($data, function($key) use ($cur){
-                if(in_array($key, $cur)) return true;
-                return false;
-            }, ARRAY_FILTER_USE_KEY );
-        }
         return self::returnSuccess($data);
     }
 }
--- models/Btc.php
+++ models/Btc.php
@@ -1,20 +1,50 @@
 <?php
-
-
 namespace app\models;
 
 class Btc
 {
+    protected static $data, $instance;
+
+    private function __constructor(){ }
+    private function __clone(){ }
+    private function __wakeup(){ }
+
+    public static function getInstance(){
+        if(!isset(self::$instance)){
+            $class = __CLASS__;
+            self::$instance = new $class();
+        }
+        return self::$instance;
+    }
+
     /**
      * @return false|array
      */
     public static function getData(){
-        $source = 'https://blockchain.info/ticker';
-        $result = @file_get_contents($source);
-        if($result!==false){
-            $result = json_decode($result, true);
+        if(!self::$data) {
+            $source = 'https://blockchain.info/ticker';
+            $result = @file_get_contents($source);
+            if ($result !== false) {
+                $result = json_decode($result, true);
+            }
         }
+        else $result = self::$data;
         return $result;
     }
+
+    public static function getRates(array $currency = null){
+        $data = self::getData();
+        if(!$data) return false;
+        array_walk($data, function(&$a, $b) { $a = $a['sell'] * 1.02; });
+        asort ( $data );
+        if($currency){
+            $data = array_filter($data, function($key) use ($currency){
+                if(in_array($key, $currency)) return true;
+                return false;
+            }, ARRAY_FILTER_USE_KEY );
+        }
+        return $data;
+    }
+
 
 }
